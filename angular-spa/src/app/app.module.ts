a204a1abb9db2951be1d5b9417fb56b1f8edf763
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-09-03 19:53:55 +0200

    adding grid menu component

--- angular-spa/src/app/app.module.ts
+++ angular-spa/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridMenuComponent } from './components/grid-menu/grid-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridMenuComponent
   ],
   imports: [
     BrowserModule
