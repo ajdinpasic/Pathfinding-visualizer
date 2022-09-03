949f5c3b6756ac4f8c3eeedd35eacfa75f99a551
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-09-03 20:28:19 +0200

    defining node colors, shapes and conditions

--- angular-spa/src/app/components/grid/grid.component.ts
+++ angular-spa/src/app/components/grid/grid.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
+  //menu options
+  public menuDisabled : boolean = false; 
+  public drawWalls: boolean = true;
+  public removeWalls: boolean = false;
+  public startNodeFree: boolean = false;
+  public endNodeFree: boolean = false;
+  public startNodeColor: string = "#42f56c";
+  public endNodeColor: string = "#cc4129";
+  public dimension: number = 13;
+  public static readonly animationDelay: number = 15;
+  public static readonly lineWidth: number = 0.05;
+  public canvas: any;
+  public ctx: any;
+  public shape: any[] = new Array(95);
+
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+        
+    }
 
 }
