b7570de647436379cad221b794d4b78cef903316
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-09-04 10:32:03 +0200

    changing dropdown options

--- angular-spa/src/app/components/grid-menu/grid-menu.component.ts
+++ angular-spa/src/app/components/grid-menu/grid-menu.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridMenuComponent implements OnInit {
 
+  public action : string = "Select action";
+  public algo : string = "Select algorithm";
+  public menuDisabled: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateAction(type: number): void {
+    switch(type) {
+      case 1:
+        this.action = "Add walls";
+        break;
+      case 2:
+        this.action = "Remove walls";
+        break;
+      case 3:
+        this.action = "Change start node";
+        break;
+      case 4:
+        this.action = "Change start node";
+        break;
+    }
+  }
+
+  updateAlgo(type: number): void {
+    switch(type) {
+        case 1:
+          this.algo = "A*";
+          break;
+        case 2:
+          this.algo = "Dijkstra";
+          break;
+        case 3:
+          this.algo = "Breadth-first search";
+          break;
+        }
   }
 
 }
